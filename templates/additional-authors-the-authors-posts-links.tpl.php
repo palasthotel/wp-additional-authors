96049369b220dd6767211de657124e33b8ba90d4
Kim Meyer <kim.meyer@palasthotel.de>
2017-02-09 15:18:57 +0100

    Small template update

--- templates/additional-authors-the-authors-posts-links.tpl.php
+++ templates/additional-authors-the-authors-posts-links.tpl.php
@@ -3,7 +3,7 @@
  * @var \AdditionalAuthors\Render $this
  * @var array $additional_authors_ids
  */
+
 foreach ( $additional_authors_ids as $author_id ) {
 	do_action(AdditionalAuthors::ACTION_THE_AUTHOR_POSTS_LINK, $author_id);
 }
-
