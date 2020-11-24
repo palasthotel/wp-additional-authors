b3879337db74f4e8a85ac40d1e37d4ba8d034401
EdwardBock <edward.bock@palasthotel.de>
2020-11-24 09:29:02 +0100

    webpack config update

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
-        main: './src/meta-box/main.jsx',
+        "meta-box": './src/meta-box.js',
     },
     output: {
         path: path.resolve(__dirname) + '/public/dist/.',
-        filename: '[name].js',
-        sourceMapFilename: '[name].map',
     },
     devtool: 'source-map',
     module: {
@@ -24,9 +23,28 @@ module.exports = {
                         ],
                     }
                 },
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+    ],
     externals: {
         react: 'React',
         'react-dom': 'ReactDOM',
