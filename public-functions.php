422289e5884249133e6db677d0caae4f0e41f2be
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-04-30 13:12:52 +0200

    chore: build

--- public-functions.php
+++ public-functions.php
@@ -59,3 +59,54 @@ function additional_authors_solr_author_ids($author_ids, $post_id){
 }
 add_filter('solr_index_update_author_ids', 'additional_authors_solr_author_ids', 10, 2);
 
+
+/**
+ * Helpers for gutenberg blocks
+ * replacements for core functions
+ */
+function additional_authors_get_the_author(){
+    $additional_author_archive_object = get_queried_object();
+    return apply_filters( 'the_author', is_object( $additional_author_archive_object ) ? $additional_author_archive_object->display_name : '' );
+}
+
+function additional_authors_get_archive_author_title() {
+    $title  = __( 'Archives' );
+    $prefix = '';
+
+    if ( is_author() ) {
+        $title  = additional_authors_get_the_author();
+        $prefix = _x( 'Author:', 'author archive title prefix' );
+    }
+
+    $original_title = $title;
+
+    /**
+     * Filters the archive title prefix.
+     *
+     * @since 5.5.0
+     *
+     * @param string $prefix Archive title prefix.
+     */
+    $prefix = apply_filters( 'get_the_archive_title_prefix', $prefix );
+    if ( $prefix ) {
+        $title = sprintf(
+        /* translators: 1: Title prefix. 2: Title. */
+            _x( '%1$s %2$s', 'archive title' ),
+            $prefix,
+            '<span>' . $title . '</span>'
+        );
+    }
+
+    /**
+     * Filters the archive title.
+     *
+     * @since 4.1.0
+     * @since 5.5.0 Added the `$prefix` and `$original_title` parameters.
+     *
+     * @param string $title          Archive title to be displayed.
+     * @param string $original_title Archive title without prefix.
+     * @param string $prefix         Archive title prefix.
+     */
+    return apply_filters( 'get_the_archive_title', $title, $original_title, $prefix );
+}
+
--- src-blocks/ph-aa-author-archive-title/render.php
+++ src-blocks/ph-aa-author-archive-title/render.php
@@ -1,55 +1,8 @@
 <?php
-
-
-function additional_authors_get_the_author(){
-    $additional_author_archive_object = get_queried_object();
-    return apply_filters( 'the_author', is_object( $additional_author_archive_object ) ? $additional_author_archive_object->display_name : '' );
-}
-
-function additional_authors_get_archive_author_title() {
-    $title  = __( 'Archives' );
-    $prefix = '';
-
-    if ( is_author() ) {
-        $title  = additional_authors_get_the_author();
-        $prefix = _x( 'Author:', 'author archive title prefix' );
-    }
-
-    $original_title = $title;
-
-    /**
-     * Filters the archive title prefix.
-     *
-     * @since 5.5.0
-     *
-     * @param string $prefix Archive title prefix.
-     */
-    $prefix = apply_filters( 'get_the_archive_title_prefix', $prefix );
-    if ( $prefix ) {
-        $title = sprintf(
-        /* translators: 1: Title prefix. 2: Title. */
-            _x( '%1$s %2$s', 'archive title' ),
-            $prefix,
-            '<span>' . $title . '</span>'
-        );
-    }
-
-    /**
-     * Filters the archive title.
-     *
-     * @since 4.1.0
-     * @since 5.5.0 Added the `$prefix` and `$original_title` parameters.
-     *
-     * @param string $title          Archive title to be displayed.
-     * @param string $original_title Archive title without prefix.
-     * @param string $prefix         Archive title prefix.
-     */
-    return apply_filters( 'get_the_archive_title', $title, $original_title, $prefix );
-}
-
 $is_archive = is_archive();
 $title = "";
 
+$additional_author_archive_title = '';
 if ( $is_archive ) {
     $show_prefix = isset( $attributes['showPrefix'] ) ? $attributes['showPrefix'] : true;
     if ( ! $show_prefix ) {
