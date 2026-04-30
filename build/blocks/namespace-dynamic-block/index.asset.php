ed68e164ce485005b551428f9f466d6379e362d6
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-04-30 09:40:47 +0200

    chore: build

--- build/blocks/namespace-dynamic-block/index.asset.php
+++ build/blocks/namespace-dynamic-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '3bac036fcba8dc80751a');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '237c005bbc367cc11702');
--- build/blocks/namespace-static-block/index.asset.php
+++ build/blocks/namespace-static-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '2c714ecb9eaaf8d875f7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a9a821b397a11cf6fb32');
