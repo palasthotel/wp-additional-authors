abfd5ed170e9cc275fb1921516b866da7ca94197
Lucas Regalar <lucas.regalar@palasthotel.de>
2026-04-30 13:15:30 +0200

    fix(gutenberg-blocks): function redeclaration and undefined property

--- build/blocks/namespace-static-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a9a821b397a11cf6fb32');
--- src-blocks/namespace-dynamic-block/render.php
+++
@@ -1,1 +0,0 @@
-<h1>This is rendered dynamically.</h1>
